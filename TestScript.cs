fcb64dbe3493261697d191f58820af6a756eef92
Eniola <enioladekeye@gmail.com>
2026-02-10 20:50:04 +0000

    apples

--- TestScript.cs
+++ TestScript.cs
@@ -1,1 +1,11 @@
+using System;
 
+class TestScript
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your name: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Hello "+ username + "!");
+    }
+}
